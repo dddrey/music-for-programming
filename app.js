34df81fa9cab2801967c9db0d9c3194f35ed2823
Andrushkins <drachyov.andrew@gmail.com>
2017-06-15 00:53:50 +0300

    syntax fixes

--- app.js
+++ app.js
@@ -1,64 +1,64 @@
-'use strict'
+'use strict';
 
-const request = require('request')
-const DomParser = require('dom-parser')
-const parser = new DomParser()
-const Promise = require('bluebird')
-const fs = Promise.promisifyAll(require('fs'))
-const ProgressBar = require('progress')
+const request = require('request');
+const DomParser = require('dom-parser');
+const parser = new DomParser();
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const ProgressBar = require('progress');
 
-const webpage = 'http://musicforprogramming.net'
-const url = 'https://datashat.net/music_for_programming_'
-const extension = '.mp3'
+const webpage = 'http://musicforprogramming.net';
+const url = 'https://datashat.net/music_for_programming_';
+const extension = '.mp3';
 
 request(webpage, (err, response, html) => {
   if (!err && response.statusCode === 200) {
     if (!fs.existsSync('music')){
       fs.mkdirSync('music');
     }
-    getItemsList(html)
+    getItemsList(html);
   } else {
-    handleError(err)
+    handleError(err);
   }
 })
 
 const getItemsList = html => {
-  console.log("Getting tracks id's...")
+  console.log("Getting tracks id's...");
 
   let parsedHTML = parser.parseFromString(html),
       tracks = parsedHTML.getElementById('episodes'),
       tracksArr = tracks.getElementsByTagName('a'), complitedUrl,
       urlArr = [];
 
-  tracksArr.push(tracks.getElementsByClassName('selected')[0])
+  tracksArr.push(tracks.getElementsByClassName('selected')[0]);
 
   tracksArr.forEach( (item, i) => {
     let trackName = item.innerHTML,
         trackNumber = i+1, trackNameForUrl;
 
     trackName = trackName.substring(trackName.indexOf(':')+2);
-    trackNameForUrl = trackName.split(' ').join('_').split('+').join('and')
+    trackNameForUrl = trackName.split(' ').join('_').split('+').join('and');
 
     complitedUrl = url + trackNumber + '-' + trackNameForUrl.toLowerCase() + extension;
-    urlArr.push(complitedUrl)
+    urlArr.push(complitedUrl);
   })
-  console.log('Downloading ' + urlArr.length + ' tracks...')
-  downloadTracks(urlArr)
+  console.log('Downloading ' + urlArr.length + ' tracks...');
+  downloadTracks(urlArr);
 }
 
 const handleError = err => {
-  console.log("Whooops ! Looks like you're having an error: ", err)
+  console.log("Whooops ! Looks like you're having an error: ", err);
 }
 
 const downloadTrack = track => {
-  let name = track.match(/\d+-[a-z_]+\.mp3/g)
-  name = name ? name[0] : track.split('-')[1]
+  let name = track.match(/\d+-[a-z_]+\.mp3/g);
+  name = name ? name[0] : track.split('-')[1];
 
   return new Promise( (resolve, reject) => {
-    let msg, bar
+    let msg, bar;
 
     const handleFileExists = (a, b) => {
-      console.log(name + ' exists, skipping download...')
+      console.log(name + ' exists, skipping download...');
       resolve();
     }
 
@@ -78,17 +78,17 @@ const downloadTrack = track => {
         })
         .on('end', resolve)
         .pipe(fs.createWriteStream(`./music/${name}`))
-    }
+    };
 
     fs.statAsync(name)
-      .then(handleFileExists, handleFileDoesntExist)
-  })
-}
+      .then(handleFileExists, handleFileDoesntExist);
+  });
+};
 
 const downloadTracks = tracks => {
   return Promise.reduce(tracks, (total, track) => {
     return downloadTrack(track).then( () => {
-      return total + 1
-    })
-  }, 0)
-}
+      return total + 1;
+    });
+  }, 0);
+};
